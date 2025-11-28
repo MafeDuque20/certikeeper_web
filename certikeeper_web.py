d7afede844eee6c7badff1af756276c004a4430a
MafeDuque20 <mafeduri20@gmail.com>
2025-11-28 11:20:06 -0500

    Update certikeeper_web.py

--- certikeeper_web.py
+++ certikeeper_web.py
@@ -10,7 +10,7 @@ from datetime import datetime
 # CONFIGURACIÓN DE PÁGINA
 # =========================
 st.set_page_config(
-    page_title="CertiKeeper Web",
+    page_title="f Web",
     page_icon="🔐",
     layout="wide",
     initial_sidebar_state="expanded"
