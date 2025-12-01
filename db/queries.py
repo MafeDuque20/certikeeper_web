f8d34c77146ce129c71075a32e13cb798415d39b
MafeDuque20 <mafeduri20@gmail.com>
2025-12-01 12:04:09 -0500

    Update queries.py

--- db/queries.py
+++ db/queries.py
@@ -3,12 +3,10 @@ from db.connection import get_connection
 def registrar_envio(nombre_archivo, base, curso, fecha_envio):
     conn = get_connection()
     cursor = conn.cursor()
-
     cursor.execute("""
         INSERT INTO historial (nombre_archivo, base, curso, fecha_envio)
         VALUES (%s, %s, %s, %s);
     """, (nombre_archivo, base, curso, fecha_envio))
-
     conn.commit()
     cursor.close()
     conn.close()
@@ -16,11 +14,8 @@ def registrar_envio(nombre_archivo, base, curso, fecha_envio):
 def obtener_historial():
     conn = get_connection()
     cursor = conn.cursor()
-
     cursor.execute("SELECT * FROM historial ORDER BY fecha_envio DESC;")
     rows = cursor.fetchall()
-
     cursor.close()
     conn.close()
-
     return rows
