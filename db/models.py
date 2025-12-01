dd435da9bfb23da6e24decb72dfd6e627937cec1
MafeDuque20 <mafeduri20@gmail.com>
2025-12-01 10:52:59 -0500

    Create models.py

---
+++ db/models.py
@@ -0,0 +1,19 @@
+from db.connection import get_connection
+
+def create_tables():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS historial (
+            id SERIAL PRIMARY KEY,
+            nombre_archivo TEXT,
+            base TEXT,
+            curso TEXT,
+            fecha_envio TIMESTAMP
+        );
+    """)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
